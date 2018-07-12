9765d593ec214d23aef91028f041d586abcbb8e4
Dragos Tirlungeanu <Dragos@Mackie-Pro.local>
2018-07-12 17:54:18 +0100

    just lost half a day because I did not restarted my server

---
+++ public/controller.js
@@ -0,0 +1,85 @@
+
+document.getElementById('getVehicleInfo').addEventListener('click', getVehicleInfo);
+	function getVehicleInfo(){
+		console.log(123);
+		fetch('http://localhost:3000/vehicles')
+
+		// .then(function(res){
+		// 	return res.json();
+		// })
+		// .then(function(data){
+		// 	console.log(data);
+		// });
+
+		.then((res) => res.json())
+		.then((data) => {
+			let output = '<h2 class="jumbotron">Vehicles</h2>'
+			data.forEach(function(vehicle){ // clearly a loop
+				// implementing hyper text into javascript
+				output += ` 
+					<ul class="jumbotron col-lg-12">
+						<li class="col-lg-4"><h4> ID: ${vehicle.id} </h4></li> 
+						<li class="col-lg-4"><h4> Name: ${vehicle.vehicleCapDetails.capModelName} </h4></li>
+						<li class="col-lg-4"><h4> Vehicle year: ${vehicle.year} </h4></li>
+					</ul>
+				`; //adding on or appending (if my spelling is correct at this hour)				
+			});
+			document.getElementById('output').innerHTML = output;
+		});
+	}
+
+	// document.getElementById('AudiAone').addEventListener('click', AudiAone);
+	// function AudiAone(){
+	// 	console.log(123);
+	// 	fetch('http://localhost:3000/vehicles')
+
+	// 	// .then(function(res){
+	// 	// 	return res.json();
+	// 	// })
+	// 	// .then(function(data){
+	// 	// 	console.log(data);
+	// 	// });
+
+	// 	.then((res) => res.json())
+	// 	.then((data) => {
+	// 		let output = '<h2 class="jumbotron">Audi A1 Results</h2>'
+	// 		data.forEach(function(vehicle){ // clearly a loop
+	// 			// implementing hyper text into javascript
+	// 			output += ` 
+	// 				<ul class="jumbotron col-lg-12">
+	// 					<li class="col-lg-4"><h4> ID: ${vehicle.id} </h4></li> 
+	// 					<li class="col-lg-4"><h4> Name: ${vehicle.year} </h4></li>
+	// 					<li class="col-lg-4"><h4> Model Name: ${vehicle.vehicleCapDetails.capMakeName} </h4></li>
+	// 				</ul>
+	// 			`; //adding on or appending (if my spelling is correct at this hour)				
+	// 		});
+	// 		document.getElementById('output').innerHTML = output;
+	// 	})
+	// 	.catch((err) => console.log(err))
+	// }
+
+
+
+
+	document.getElementById('getResults').addEventListener('submit', getResults);
+	function getResults(e){
+		e.preventDefault();
+
+		let title = document.getElementById('title').value;
+
+		fetch('http://localhost:3000/vehicles', {
+			method:'GET',
+			headers: { // settin it as an object
+				'Accept': 'application/json, text/plain, */*', // last accept form is anything from anything
+				'Content-type':'application/json'
+			}
+		})
+	
+		.then((res) => res.json())
+		.then((data) => {
+			data.forEach(function(vehicle){
+			output += `<h2 class="jumbotron"> ID: ${vehicle.id}</h2>`;
+			document.getElementById('output').innerHTML = output;
+		});
+	});
+	}
